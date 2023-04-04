29b4307b332c019c05c4873d44e2e728ed8307e7
marksikaundi <emmanuelmark117@gmail.com>
2023-04-04 23:57:25 +0200

    operators in JS

---
+++ Week1/operators.js
@@ -0,0 +1,11 @@
+/*Comparison operators in JS:
+> greater than
+< less than
+== equal
+*/
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 / 2)
+console.log(2 < 2)
+console.log(2 > 2)
+console.log(2 == 2)
